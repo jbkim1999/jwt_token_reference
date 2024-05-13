3a789c54e09d6cbfc2db445a72785fbd8e320593
JB Kim <jungbae.kim@u.nus.edu>
2024-05-13 19:18:08 +0900

    Add validation methods

--- src/main/java/com/jbjbjb/security/config/JwtService.java
+++ src/main/java/com/jbjbjb/security/config/JwtService.java
@@ -1,21 +1,26 @@
 package com.jbjbjb.security.config;
 
 import io.jsonwebtoken.Claims;
-
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
+
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
 
     // string -> encoded base 64 -> generate key by applying an appropriate HMAC algorithm
+    // DON'T EXPOSE THIS IN A REAL PROJECT
     private static final String SECRET_KEY_STRING = "jbjbjb"; //
 
     public String encodeBase64SecretKey(String secretKey) {
@@ -26,6 +31,24 @@ public class JwtService {
         byte[] keyBytes = Decoders.BASE64.decode(base64EncodedSecretKey);
 
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 10))
+                .signWith(getKeyFromBase64EncodedKey(encodeBase64SecretKey(SECRET_KEY_STRING)))
+                .compact();
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
     }
 
     private Claims extractAllClaims(String token) {
@@ -44,5 +67,18 @@ public class JwtService {
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
     }
 }
